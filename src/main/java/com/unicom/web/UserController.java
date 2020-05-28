95d0d5e0d1666e32eaa6f461435dcc8916d45290
liukai <zhizhufan@foxmail.com>
2020-05-28 14:28:56 +0800

    update UserController

--- src/main/java/com/unicom/web/UserController.java
+++ src/main/java/com/unicom/web/UserController.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,14 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-
-    @PostMapping("/oauth2/api/user_info")
+    @GetMapping("/oauth2/api/user_info")
     public Authentication getUserInfo(@AuthenticationPrincipal Authentication authentication) {
-
-
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-
-//        log.info("user: {}", user);
 
         return authentication;
     }
